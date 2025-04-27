3a1a282c61cca35b4e388649dc44ec97eda156fe
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-04-27 18:58:49 -0300

    Starting a new project

---
+++ src/main/java/org/furia/chatbot/FuriaApplication.java
@@ -0,0 +1,15 @@
+package org.furia.chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaApplication {
+
+    public static void main (String[] args) {
+
+        SpringApplication.run(FuriaApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/org/furia/chatbot/FuriaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.furia.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
