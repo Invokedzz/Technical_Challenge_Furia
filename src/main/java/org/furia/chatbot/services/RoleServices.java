3db9102d05daec1e7eee5b61ec55b7858ed18a4f
Samuel Nóbrega <samuelnobrega902@gmail.com>
2025-05-01 22:24:18 -0300

    end

--- src/main/java/org/furia/chatbot/services/RoleServices.java
+++ src/main/java/org/furia/chatbot/services/RoleServices.java
@@ -1,6 +1,7 @@
 package org.furia.chatbot.services;
 
 import lombok.RequiredArgsConstructor;
+import org.furia.chatbot.model.Roles;
 import org.furia.chatbot.repository.RoleRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,12 @@ import org.springframework.stereotype.Service;
 public class RoleServices {
 
     private final RoleRepository roleRepository;
+
+    public void createRole () {
+
+        roleRepository.save(new Roles(1L, "ROLE_CLIENT"));
+
+    }
 
     public void insertRoleInUser (Long userId) {
 
